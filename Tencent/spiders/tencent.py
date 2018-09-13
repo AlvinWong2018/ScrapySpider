236de7de0dc0e162cc01adfe907f315e012de10b
AlvinWong2018 <lzzxhhh@126.com>
2018-09-13 21:29:37 +0800

    push first time

---
+++ Tencent/spiders/tencent.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Tencent.items import TencentItem
+
+
+class TencentSpider(scrapy.Spider):
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+
+    baseUrl = 'https://hr.tencent.com/position.php?&start=0'
+
+    start_urls = [baseUrl]
+
+    def parse(self, response):
+        node_list = response.xpath("//tr[@class='even'] | //tr[@class='odd']")
+
+        for node in node_list:
+            item = TencentItem()
+            item['positionName'] = node.xpath("./td[1]/a/text()").extract()[0].encode("utf-8")
+            item['positionLink'] = node.xpath("./td[1]/a/@href").extract()[0].encode("utf-8")
+
+            if len(node.xpath("./td[2]/text()")):
+                item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            else:
+                item['positionType'] = "NULL"
+
+            item['peopleNumber'] = node.xpath("./td[3]/text()").extract()[0].encode("utf-8")
+            item['workLocation'] = node.xpath("./td[4]/text()").extract()[0].encode("utf-8")
+            item['publishTime'] = node.xpath("./td[5]/text()").extract()[0].encode("utf-8")
+
+            yield item
+
+        if len(response.xpath("//a[@id='next' and @class='noactive']")) == 0:
+            url = response.xpath("//a[@id='next']/@href").extract()[0]
+            yield scrapy.Request('https://hr.tencent.com/' + url, callback=self.parse)
+
